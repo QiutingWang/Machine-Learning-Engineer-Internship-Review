45bc0636138e9960b6eba9147ab290aa484ea398
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-06-21 22:25:01 +0800

    Add files via upload

--- GoogleNet.py
+++ GoogleNet.py
@@ -17,9 +17,50 @@ class BasicConv2d(nn.Module):
         return x
 
 
+
 class Inception(nn.Module):
     def __init__(self, in_channels, filter11, filter33reduce, filter33, filter55reduce, filter55, pool_proj):
         self.branch1=BasicConv2d(in_channels, filter11, kernel_size=1)
+        self.branch2=nn.Sequential(
+            BasicConv2d(in_channels, filter33reduce, kernel_size=1),
+            BasicConv2d(filter33, kernel_size=1, padding=1)
+        )
+        self.branch3=nn.Sequential(
+            BasicConv2d(in_channels, filter55reduce, kernel_size=1, padding=1),
+            BasicConv2d(filter55reduce, filter55, kernel_size=5, padding=2)
+        )
+        self.branch4=nn.Sequential(
+            nn.MaxPooling2d(kernel_size=3, stride=1, padding=1),
+            BasicConv2d(in_channels, pool_proj, kernel_size=1)
+        )
+    
+    def forward(self, x):
+        branch1=self.branch1(x)
+        branch2=self.branch2(x)
+        branch3=self.branch3(x)
+        branch4=self.branch4(x)
+        outputs=[branch1, branch2, branch3, branch4]
+        return torch.cat(outputs, 1)
+
+
+
+class InceptionAux(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(InceptionAux, self).__init__()
+        self.averagePool=nn.AvgPool2d(kernel_size=5, stride=3)
+        self.conv=BasicConv2d(in_channels, 128, kernel_size=1)
+        self.FC1=nn.Linear(2048, 1024)
+        self.FC2=nn.Linear(1024, num_classes)
+    
+    def forward(self, x):
+        x=self.averagePool(x)
+        x=self.conv(x)
+        x=torch.flatten(x, 1)
+        x=F.dropout(x, 0.5, training=self.training)
+        x=F.relu(self.FC1(x), inplace=True)
+        x=F.dropout(x, 0.5, training=self.training)
+        x=self.FC2(x)
+        return x
 
 
 
@@ -44,3 +85,57 @@ class GoogLeNet(nn.Module):
         self.maxpool4=nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
         self.inception5a=Inception(832, 256, 160, 320, 32, 128, 128)
         self.inception5b=Inception(832, 394, 192, 384, 48, 128, 128)
+        if self.aux_logits:
+            self.aux1=InceptionAux(512, num_classes)
+            self.aux2=InceptionAux(528, num_classes)
+        self.avgpool=nn.AdaptiveAvgPool2d((1,1)) #specify pooling size (Height,Wide)
+        self.dropout=nn.Dropout(0.4)
+        self.FC=nn.Linear(1024, num_classes)
+        if init_weights:
+            self._initialize_weights()
+    
+    def forward(self, x):
+        x=self.conv1(x)
+        x=self.maxpool1(x)
+        x=self.conv2(x)
+        x=self.conv3(x)
+        x=self.maxpool2(x)
+        x=self.inception3a(x)
+        x=self.inception3b(x)
+        x=self.maxpool3(x)
+        x=self.inception4a(x)
+        if self.training and self.aux_logits:
+            aux1=self.aux1(x)
+        x=self.inception4b(x)
+        x=self.inception4c(x)
+        x=self.inception4d(x)
+        if self.training and self.aux_logits:
+            aux2=self.aux2(x)
+        x=self.inception4e(x)
+        x=self.maxpool4(x)
+        x=self.inception5a(x)
+        x=self.inception5b(x)
+        x=self.avgpool(x)
+        x=torch.flatten(x, 1)
+        x=self.dropout(x)
+        x=self.FC(x)
+        if self.training and self.aux_logits:
+            return x, aux1, aux2
+        return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+        
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+GoogLeNet=GoogLeNet(num_classes=5, aux_logits=True, init_weights=True)
+GoogLeNet=GoogLeNet.to(device)
+loss_function=nn.CrossEntropyLoss()
+optimizer=optim.Adam(GoogLeNet.parameters(), lr=0.0003)
